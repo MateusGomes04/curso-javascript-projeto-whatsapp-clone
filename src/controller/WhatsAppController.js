ef41acb016e503b577bd4d7f9ab9be070c111805
MateusGomes04 <mateusgomesc04@gmail.com>
2022-10-17 08:52:22 -0300

    authentic firebase

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -13,10 +13,34 @@ export class WhatsAppController {
         console.log("WhatsAppController OK");
 
         this._firebase = new Firebase();
+        this.initAuth();
         this.elementsPrototype();
         this.loadElements();
         this.initEvents();
     }
+
+    initAuth(){
+
+        this._firebase.initAuth().then(response=>{
+
+            
+
+          this._user = response.user;
+
+          this.el.appContent.css({
+
+            display:"flex"
+            
+          });
+
+        })
+
+        .catch(err=>{
+            console.log(err);
+        });
+
+    }
+
 
     loadElements() {
 
